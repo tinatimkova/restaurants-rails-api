3078890cc813a6a3ccfac5a7346efb58d3979a22
timosha <tinaatimkovaa@gmail.com>
2020-01-30 11:45:00 -0500

    Add 'editable' method to restaurant_serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,4 +1,8 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :address, :description, :rating
   has_one :user
+
+  def editable
+    scope == object.user
+  end
 end
