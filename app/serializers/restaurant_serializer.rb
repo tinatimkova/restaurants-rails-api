87dc9a39c139e9c863c4a530b990e01230b6d189
timosha <tinaatimkovaa@gmail.com>
2020-02-03 13:11:56 -0500

    Add editable to restaurant_serializer
    Fixed the issue with action buttons

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,4 +1,8 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :description, :rating
+  attributes :id, :name, :address, :description, :rating, :editable
   has_one :user
+
+  def editable
+    scope == object.user
+  end
 end
